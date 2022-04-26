b9a656c25e2e37d6440ad45a31ce0ff5874332cc
Katja <end.of.all89@gmail.com>
2022-04-26 12:33:04 +0300

    added toggle for mod mode and user mode

--- vue-app/src/components/NavBar.vue
+++ vue-app/src/components/NavBar.vue
@@ -1,22 +1,5 @@
 <template>
-<div id =navbar>
-
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">PartyTracker</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-link active" aria-current="page" href="#navbar">Home</a>
-          <a class="nav-link" href="#event-form">Add a new event</a>
-          <a class="nav-link" href="#event-table">List of events</a>
-        </div>
-      </div>
-    </div>
-  </nav>
-</div>
+<h1>test</h1>
 </template>
 
 <script>
