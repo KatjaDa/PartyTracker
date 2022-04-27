2e598ec998d1371d30a5632299a58401ff973115
Jami Hämäläinen <jami.hamalainen2@metropolia.fi>
2022-04-27 11:27:36 +0300

    Added dropdown menu to SearchEvent

--- vue-app/src/components/SearchEvent.vue
+++ vue-app/src/components/SearchEvent.vue
@@ -1,29 +1,34 @@
 <template>
   <h2>Search for events</h2>
-  <label>Min date:</label>
-  <input ref="mindate" type="date" value="2022-01-01" placeholder="Ex: 2022-01-01"/>
 
+  <span>Search criteria: </span>
+  <select>
+    <option @click="searchCriteria = 'all'">All</option>
+    <option @click="searchCriteria = 'date'">Date</option>
+    <option @click="searchCriteria = 'city'">City</option>
+  </select>
 
-  <br><label>Max date:</label>
-  <input ref="maxdate" type="date" value="2022-12-01" placeholder="2022-12-01"/>
+  <section v-if="searchCriteria == 'date'">
+    <label>Min date:</label>
+    <input ref="mindate" type="date" value="2022-01-01" placeholder="Ex: 2022-01-01"/>
 
-  <label>Search by city:</label>
-  <input ref="city" type="text" value="Helsinki" placeholder="Ex. Helsinki"/>
+    <br><label>Max date:</label>
+    <input ref="maxdate" type="date" value="2022-12-01" placeholder="2022-12-01"/>
+  </section>
 
+  <section v-if="searchCriteria == 'city'">
+    <label>Search by city:</label>
+    <input ref="city" type="text" value="Helsinki" placeholder="Ex. Helsinki"/>
+  </section>
 
-  <br>
-  <button @click="searchEventsByDate()">Search events by date</button>
-
-  <button @click="searchEventsByCity()">Search events by city</button>
+  <button @click="searchEvents()">Search events</button>
 </template>
 
 <script>
 export default {
   name: "search-event",
 
-
   methods: {
-
     searchEventsByDate(){
       let mindate = this.$refs.mindate.value;
       let maxdate = this.$refs.maxdate.value;
@@ -33,14 +38,30 @@ export default {
     searchEventsByCity(){
       let city = this.$refs.city.value;
       this.$emit('search2:event', city);
+    },
+
+    searchEvents() {
+      switch (this.searchCriteria) {
+        case 'all':
+          this.$emit('addall:events');
+          break;
+        case 'date':
+          this.searchEventsByDate();
+          break;
+        case 'city':
+          this.searchEventsByCity();
+          break;
+      }
+    }
+  },
+
+  data() {
+    return {
+      searchCriteria: 'all'
     }
   }
 }
 
-
-
 </script>
-
 <style scoped>
-
 </style>
